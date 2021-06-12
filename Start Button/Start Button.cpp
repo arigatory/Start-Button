188364dac2c7aed4f5002b20e4b5d2b727156e3d
Ivan MSI <arigatory@gmail.com>
2021-06-13 00:29:21 +0300

    correct size of the start button

--- Start Button/Start Button.cpp
+++ Start Button/Start Button.cpp
@@ -2,93 +2,98 @@
 //
 
 #include "framework.h"
-#include "Start Button.h"
+// Start Button.cpp : Defines the entry point for the application.
+//
 
+#include "Start Button.h"
 #define MAX_LOADSTRING 100
 
 // Global Variables:
 HWND hWnd;
 HBITMAP startBitmap;
-HINSTANCE hInst;                                // current instance
-WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
-WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+HINSTANCE hInst;								// current instance
+TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
 
 // Forward declarations of functions included in this code module:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
 
 void DrawStartButton()
 {
-    int i;
-    int toppos = GetSystemMetrics(SM_CYSCREEN) - 53;
-    HANDLE h;
+ int i;
+ HANDLE h;
+ int toppos=GetSystemMetrics(SM_CYSCREEN)-53;
 
-    ShowWindow(hWnd, SW_SHOW);
-    SetWindowPos(hWnd, HWND_TOPMOST, 4, toppos, 60, 50, SWP_SHOWWINDOW);
-    UpdateWindow(hWnd);
-    h=CreateEvent(0, true, false, "et");
+ //Отображаем окно
+ ShowWindow(hWnd, SW_SHOW);
+ //Установить верхнюю позицию окна в левый нижний угол экрана.
+ SetWindowPos(hWnd, HWND_TOPMOST, 4, toppos, 60, 50, SWP_SHOWWINDOW);
+ UpdateWindow(hWnd);
+ //Создаю пустой указатель h который буду использовать для задержки.
+ h=CreateEvent(0, true, false, "et");
 
-    for ( i = 0; i < 50; i++)
-    {
-        toppos=toppos-4;
-        SetWindowPos(hWnd, HWND_TOPMOST, 4, toppos, 60, 50, SWP_SHOWWINDOW);
-        WaitForSingleObject(h,15);
-    }
+ //Сейчас будем подымать кнопку
+ // От 1 до 80 выполнять действия от begin до end for i:=1 to 80 do
+ for (i=0; i<50; i++)
+ {
+		toppos=toppos-4;
+		SetWindowPos(hWnd, HWND_TOPMOST, 4, toppos, 60, 50, SWP_SHOWWINDOW);
+		WaitForSingleObject(h,15);//Задержка в 5 миллисекунд
+ }
 
-       for ( i = 50; i > 0; i--)
-    {
-        toppos=toppos+4;
-        SetWindowPos(hWnd, HWND_TOPMOST, 4, toppos, 60, 50, SWP_SHOWWINDOW);
-        WaitForSingleObject(h,15);
-       }
+ for (i=50; i>0; i--)
+ {
+		toppos=toppos+4;
+		SetWindowPos(hWnd, HWND_TOPMOST, 4, toppos, 60, 50, SWP_SHOWWINDOW);
+		WaitForSingleObject(h,15);//Задержка в 5 миллисекунд
+ }
 }
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+ 	// TODO: Place code here.
+	MSG msg;
+	HACCEL hAccelTable;
 
-    // TODO: Place code here.
+	// Initialize global strings
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_STARTBUTTON, szWindowClass, MAX_LOADSTRING);
 
-    // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_STARTBUTTON, szWindowClass, MAX_LOADSTRING);
+    startBitmap = (HBITMAP)::LoadImage(hInstance, 
+      MAKEINTRESOURCE(IDB_BITMAP1), IMAGE_BITMAP, 
+      0, 0, LR_DEFAULTCOLOR);
+	
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	if (!InitInstance (hInstance, nCmdShow)) 
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, (LPCTSTR)IDC_STARTBUTTON);
+
+	DrawStartButton();
     
-    startBitmap = (HBITMAP)::LoadImage(hInstance, MAKEINTRESOURCE(IDB_BITMAP1), IMAGE_BITMAP, 0, 0, LR_DEFAULTCOLOR);
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0)) 
+	{
+		DrawStartButton();
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) 
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
 
-    
-    MyRegisterClass(hInstance);
-
-    // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
-
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_STARTBUTTON));
-
-    MSG msg;
-
-
-    DrawStartButton();
-    // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        DrawStartButton();
-        
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
-
-    return (int) msg.wParam;
+	return (int) msg.wParam;
 }
 
 
@@ -98,29 +103,37 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 //  PURPOSE: Registers the window class.
 //
+//  COMMENTS:
+//
+//    This function and its usage are only necessary if you want this code
+//    to be compatible with Win32 systems prior to the 'RegisterClassEx'
+//    function that was added to Windows 95. It is important to call this function
+//    so that the application will get 'well formed' small icons associated
+//    with it.
+//
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
+	WNDCLASSEX wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.cbSize = sizeof(WNDCLASSEX); 
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_STARTBUTTON));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_STARTBUTTON);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.style			= 0;
+	wcex.lpfnWndProc	= (WNDPROC)WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, (LPCTSTR)IDI_STARTBUTTON);
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= "";
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, (LPCTSTR)IDI_SMALL);
 
-    return RegisterClassExW(&wcex);
+	return RegisterClassEx(&wcex);
 }
 
 //
-//   FUNCTION: InitInstance(HINSTANCE, int)
+//   FUNCTION: InitInstance(HANDLE, int)
 //
 //   PURPOSE: Saves instance handle and creates main window
 //
@@ -133,100 +146,70 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // Store instance handle in our global variable
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+   hWnd = CreateWindow(szWindowClass, szTitle, WS_VISIBLE,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
 
    if (!hWnd)
    {
       return FALSE;
    }
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
    int Style;
    Style = GetWindowLong(hWnd, GWL_STYLE);
-   Style = Style || WS_CAPTION;
-   Style = Style || WS_SYSMENU;
+   Style=Style || WS_CAPTION;
+   Style=Style || WS_SYSMENU;
    SetWindowLong(hWnd, GWL_STYLE, Style);
 
    return TRUE;
 }
 
 //
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//  FUNCTION: WndProc(HWND, unsigned, WORD, LONG)
 //
-//  PURPOSE: Processes messages for the main window.
+//  PURPOSE:  Processes messages for the main window.
 //
-//  WM_COMMAND  - process the application menu
-//  WM_PAINT    - Paint the main window
-//  WM_DESTROY  - post a quit message and return
+//  WM_COMMAND	- process the application menu
+//  WM_PAINT	- Paint the main window
+//  WM_DESTROY	- post a quit message and return
 //
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    int wmId, wmEvent;
-    PAINTSTRUCT ps;
-    HDC hdc;
-    HDC hdcBits;
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+	HDC hdcBits;
 
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Parse the menu selections:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
-            Rectangle(hdc, 1, 1, 10, 10);
-                hdcBits = ::CreateCompatibleDC(hdc);
-                SelectObject(hdcBits, startBitmap);
-                BitBlt(hdc, 0, 0, 60, 50, hdcBits, 0, 0, SRCCOPY);
-                DeleteDC(hdcBits);
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
-}
-
-// Message handler for about box.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	switch (message) 
+	{
+	case WM_COMMAND:
+		wmId    = LOWORD(wParam); 
+		wmEvent = HIWORD(wParam); 
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code here...
+		Rectangle(hdc, 1,1,10,10);
+		hdcBits=::CreateCompatibleDC(hdc);
+		SelectObject(hdcBits,startBitmap);
+		BitBlt(hdc, 0, 0, 60, 50, hdcBits, 0, 0, SRCCOPY);
+		DeleteDC(hdcBits);
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
 }
